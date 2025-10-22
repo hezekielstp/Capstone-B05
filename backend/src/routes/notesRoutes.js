c412bf77715b04558182149cea7f38760845579d
HezekielSitepu <hezekielsitepu@gmail.com>
2025-10-22 13:06:56 +0700

    Add : Backend

---
+++ backend/src/routes/notesRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { createNote, deleteNote, getAllNotes, updateNote } from "../controllers/notesController.js";
+
+const router = express.Router();
+
+router.get("/",getAllNotes);
+router.post ("/", createNote);
+router.put ("/:id", updateNote);
+router.delete ("/:id", deleteNote);
+
+export default router;
+
+// app.get("/api/notes", (req,res) => {
+//     res.status(200).send ("you get 10 notes");
+// });
+
+// app.post("/api/notes", (req,res) => {
+//     res.status(201).json({message : "Post created succesfully"});
+// });
+
+// app.put("/api/notes/:id", (req,res) => {
+//     res.status(200).json({message : "Post updated succesfully"});
+// });
+
+// app.delete("/api/notes/:id", (req,res) => {
+//     res.status(200).json({message : "Note deleted succesfully"});
+// });
